d330d9a3977d99f242641da83bcf02579acbedab
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2022-06-16 00:29:44 +0800

    导航菜单栏实现有图标使用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
+  <Home-header ref="HomeHeaderTheme"></Home-header>
   <router-view></router-view>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
+import HomeHeader from '@/components/HomeHeader';
+import { watch, reactive, onMounted, ref } from 'vue';
+//第一种获取target值的方式，通过vue中的响应式对象可使用toRaw()方法获取原始对象
+import { toRaw } from '@vue/reactivity'
 
 let route = useRoute();
 let router = useRouter();
-function goAbout() {
-  console.log(route, '====router');
-  router.push("/about")
-}
+
+const data = reactive({
+  theme: 'light',
+})
+const HomeHeaderTheme = ref(null);
+
+onMounted(() => {
+  // 当界面挂载出来后就会自动执行
+  console.log(HomeHeaderTheme, HomeHeaderTheme.value, toRaw(HomeHeaderTheme.value), JSON.parse(JSON.stringify(HomeHeaderTheme.value)));
+});
+//接受的是null,原因是setup执行时机比mounted早,dom还没形成
+console.log(HomeHeaderTheme.value);
+// data.theme =
+
+// watch(() => userInfo.username, (newVal, oldVal) => {
+//   console.log(newVal, '----newVal');
+//   console.log(oldVal, '----oldVal');
+// })
 </script>
 
 <style>
---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="header theme" :class="data.theme">
+    <div class="change-theme">
+      <span>切换主题:</span>
+      <a-switch :checked="data.theme === 'dark'" checked-children="Dark" un-checked-children="Light"
+        @change="changeTheme" class="switch-theme" />
+    </div>
+    <div class="logo" @click="router.push('/')">
+      <img :src="getImageUrl('@/assets/logo.png')" alt="logo">
+    </div>
+    <a-input-search v-model:value="value" placeholder="搜索商品和商品号" class="input-search" @search="onSearch" />
+
+    <a-menu v-model:selectedKeys="current" mode="horizontal" class="menu" :theme="data.theme">
+      <a-menu-item key="help">
+        <template #icon>
+          <question-circle-outlined />
+        </template>
+        帮助
+      </a-menu-item>
+      <a-menu-item key="salesRoom">
+        <template #icon>
+          <shop-outlined />
+        </template>
+        门店
+      </a-menu-item>
+      <a-sub-menu key="account">
+        <template #icon>
+          <user-outlined />
+        </template>
+        <template #title>账户</template>
+      </a-sub-menu>
+      <a-sub-menu key="buyCart">
+        <template #icon>
+          <shopping-cart-outlined />
+        </template>
+        <template #title>购物车</template>
+        <a-menu-item-group title="Item 1">
+          <a-menu-item key="setting:1">Option 1</a-menu-item>
+          <a-menu-item key="setting:2">Option 2</a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group title="Item 2">
+          <a-menu-item key="setting:3">Option 3</a-menu-item>
+          <a-menu-item key="setting:4">Option 4</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { defineComponent, ref, reactive } from 'vue';
+import { QuestionCircleOutlined, ShopOutlined, UserOutlined, ShoppingCartOutlined } from '@ant-design/icons-vue';
+import { getImageUrl } from "@/utils/resolveUrl"
+
+const router = useRouter();
+const data = reactive({
+  theme: 'light',
+  value: '',
+  current: [],
+})
+
+const changeTheme = checked => {
+  data.theme = checked ? 'dark' : 'light';
+};
+
+const onSearch = () => {
+
+}
+</script>
+<style lang='less' scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: fixed;
+  z-index: 1000000;
+
+  width: 100%;
+  height: 80px;
+  background-color: #fff;
+  box-shadow: 0 0 10px #ccc;
+  font-size: 18px;
+
+  padding: 0 20px;
+
+  .change-theme {
+    .switch-theme {
+      margin-left: 10px;
+    }
+  }
+
+  .logo {
+    margin: 0 20px;
+
+    img {
+      height: 60px;
+    }
+  }
+
+  .input-search {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    border-width: 10px;
+  }
+
+  .menu {
+    display: flex;
+    align-items: center;
+    height: 80px;
+    font-size: 16px;
+  }
+}
+</style>

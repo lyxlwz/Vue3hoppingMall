9fb133cff679cfdc8af4524e365bca797c6a22bd
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2022-06-21 22:00:21 +0800

    添加产品模型

--- src/utils/base3d.js
+++ src/utils/base3d.js
@@ -4,6 +4,8 @@ import * as THREE from "three";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 // 4. 引入轨道控制器（控制器的一种）-----围绕物体进行查看
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+// 5.引入模型解析器
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 // 3.构建需要的3d的类
 class Base3d {
@@ -12,6 +14,8 @@ class Base3d {
     this.camera; //照相机（Camera）
     this.scene; //场景（Scene）
     this.renderer; //渲染器（Renderer）
+
+    this.model; //模型
 
     //初始化
     this.init()
@@ -24,6 +28,7 @@ class Base3d {
     this.initCamera(); //初始化照相机
     this.initRenderer(); //初始化渲染器
     this.initControls(); //初始化控制器
+    this.addMesh(); //添加模型/物体
   }
   initScene() {
     this.scene = new THREE.Scene(); //初始化场景
@@ -89,6 +94,23 @@ class Base3d {
       this.scene.background = texture;
       this.scene.environment = texture;
     })
+  }
+
+  // 设置模型
+  setModel(modelName) {
+    return new Promise((resolve, reject) => {
+      // 初始化解析器
+      const loader = new GLTFLoader().setPath('files/gltf/')
+      loader.load(modelName, (gltf) => {
+        //场景的第一个就是模型
+        this.model = gltf.scene.children[0]
+        this.scene.add(this.model)
+        resolve(modelName + '模型添加成功')
+      })
+    })
+  }
+  addMesh() {
+    this.setModel('bag2.glb')
   }
 
   // 渲染界面 
